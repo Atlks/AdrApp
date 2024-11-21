08152ead0ce14a7d2d37538a1845c0ca42cffc7b
dms <dddmi@ff.xom>
2024-11-22 03:03:23 +0800

    ingr tmr

--- app/src/main/java/comx/pkg/MyNotificationListenerService.kt
+++ app/src/main/java/comx/pkg/MyNotificationListenerService.kt
@@ -83,6 +83,9 @@ class MyNotificationListenerService : NotificationListenerService(), TextToSpeec
         if(title=="没有标题" && text=="没有内容")
             return
 
+        if(title.toLowerCase()=="timer")
+            return
+
         // 使用 TTS 阅读通知内容
         speakOut(message)
     }
@@ -102,23 +105,7 @@ class MyNotificationListenerService : NotificationListenerService(), TextToSpeec
       //  textToSpeech?.speak(message, TextToSpeech.QUEUE_FLUSH, null, null)
     }
 
-    // 语言检测方法
-    private fun detectLanguage(text: String): Locale {
-        return if (text.any { it.isCJKCharacter() }) {
-            // 如果包含中文字符，使用中文
-            Locale.CHINESE
-        } else {
-            // 否则使用英文
-            Locale.ENGLISH
-        }
-    }
 
-    // 扩展函数：检测是否为 CJK（中日韩）字符
-    private fun Char.isCJKCharacter(): Boolean {
-        return this.code in 0x4E00..0x9FFF ||  // 常用汉字
-                this.code in 0x3400..0x4DBF ||  // 扩展汉字 A
-                this.code in 0x20000..0x2A6DF   // 扩展汉字 B
-    }
     override fun onInit(status: Int) {
         if (status == TextToSpeech.SUCCESS) {
             textToSpeech?.language = Locale.CHINESE
